ffb6e994ddcb92e6e1bca0d19fb2ce60dc7454a8
Jaro111 <jarokark@protonmail.com>
2026-02-28 01:12:58 +0000

    vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 8080, // Zmiana portu na 3000, jeśli masz konflikt z innymi aplikacjami
+  },
+  preview: {
+    port: 4173, // Możesz ustawić inny port dla wersji produkcyjnej, jeśli jest konflikt
+  },
+});
