ed6ba385327d2ba14c39728b72796f9583822298
Jaro111 <jarokark@protonmail.com>
2026-03-05 16:38:35 +0000

    Chore: Credit chceck btn

--- src/common/fetchOdd.js
+++ src/common/fetchOdd.js
@@ -15,5 +15,20 @@ export const fetchOddApi = async (sport, regions, markets) => {
   });
 
   const data = await response.json();
+  const res = await data.credits;
+  return res;
+};
+
+// Credits check
+export const checkCredits = async () => {
+  const response = await fetch(`${url}/creditCheck`, {
+    method: "GET",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await response.json();
   return data;
 };
